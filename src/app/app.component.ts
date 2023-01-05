109318bcb2507641b2369878392d49b35034aabb
AnishaKhatiwada <97074598+AnishaKhatiwada@users.noreply.github.com>
2023-01-05 11:31:47 +0545

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  loginForm = new FormControl();
+name:string;
+  address = [];
+ 
+  constructor() {}
+  ngOnInit() {
+    this.name = 'Template driven form';
+  }
 }
